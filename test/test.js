16eef2c35c4fe41b7d420b6d2599ab0aa4b09fe1
trong-le <trong.p.le.92@gmail.com>
2016-05-06 11:56:02 -0400

    Made functions generic

--- test/test.js
+++ test/test.js
@@ -1,6 +1,32 @@
 import test from 'blue-tape';
 import FlightStatsSource from '../lib/index.js';
-import config from './config/config';
+
+const config = {
+	schema : "abq",
+	table : "arrivals",
+	drop : true,
+	airport: 'ABQ',
+	requestedFields : [
+		'scheduledTime',
+		'scheduledDate',
+		'flight',
+		'actualTime',
+		'actualGateTime',
+		'statusCode',
+		'remarks',
+		'codesharesAsNames',
+		'airlineName',
+		'flightId'
+	],
+	timeWindowBegin : 720,
+	timeWindowEnd : 720,
+	lateMinutes : 15,
+	useRunwayTimes : false,
+	excludeCargoFlights : false,
+	utc : false,
+	appId : '123',
+	appKey : 'abc'
+};
 
 // example - make sure configuration is the same
 test('proper configuration', t => {
@@ -37,11 +63,31 @@ test('proper configuration', t => {
 // 	}
 // });
 
+test('get taxi data', async t => {
+	const source = new FlightStatsSource();
+	try {
+		const taxi = await source.getTaxiData(config);
+		t.comment(JSON.stringify(taxi));
+	} catch(err) {
+		t.comment(err);
+	}
+});
+
+test('get fids arrivals', async t => {
+	const source = new FlightStatsSource();
+	try {
+		const fidsArr = await source.fidsArrival(config);
+		t.comment(JSON.stringify(fidsArr));
+	} catch(err) {
+		t.comment(err);
+	}
+});
+
 test('get flight tracks departures by airport', async t => {
 	const source = new FlightStatsSource();
 	try {
-		const departures = await source.fidsDeparture(config);
-		t.comment('test departures');
+		const departures = await source.getActiveAirlines(config);
+		t.comment(JSON.stringify(departures));
 	} catch(err) {
 		t.comment(err);
 	}
