37ccded3e71baa5e5aa8efacae3a140c9a8a7e2d
trong-le <trong.p.le.92@gmail.com>
2016-05-10 17:15:20 -0400

    Simplified method call in onTask

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 import test from 'blue-tape';
 import FlightStatsSource from '../lib/index.js';
 import config from './test.config';
-import response from './test.response';
 
 // example - make sure configuration is the same
 test('proper configuration', t => {
@@ -14,7 +13,7 @@ test('proper configuration', t => {
 test('test arrivals flight status by airport', async t => {
 	const source = new FlightStatsSource();
 	const arrivals = await source.getFlightStatusArrivalsByAirport(config);
-})
+});
 
 test('get flight tracks departures by airport', async t => {
 	const source = new FlightStatsSource();
@@ -29,11 +28,6 @@ test('get flight tracks arrivals by airport', async t => {
 test('get taxi data', async t => {
 	const source = new FlightStatsSource();
 	const taxi = await source.getTaxiData(config);
-});
-
-test('get unique taxi data', async t => {
-	const source = new FlightStatsSource();
-	const uniqueTaxi = await source.getUniqueTaxiData(response);
 });
 
 test('get fids arrivals', async t => {
